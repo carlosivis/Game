ce12ba2caf3bcdf2ca4f2b1a9f56034cca3e80e3
Carlos Ivis <52825816+carlosivis@users.noreply.github.com>
2020-07-15 23:58:30 -0300

    Update main.js

--- main.js
+++ main.js
@@ -14,6 +14,29 @@ let charSprite
 let shot = []
 //let contShot = 0
 
+const baseValueSprite = 220
+const matrizChar = [
+    [0,0],[baseValueSprite,0],
+    [baseValueSprite*2,0],[baseValueSprite*3,0],
+
+    [0,baseValueSprite],[baseValueSprite,baseValueSprite],
+    [baseValueSprite*2,baseValueSprite],[baseValueSprite*3,baseValueSprite],
+
+    [0,baseValueSprite*2],[baseValueSprite,baseValueSprite*2],
+]
+const matrizSlime = [
+    [0,0],[baseValueSprite,0],
+    [baseValueSprite*2,0],[baseValueSprite*3,0],
+
+    [0,baseValueSprite],[baseValueSprite,baseValueSprite],
+    [baseValueSprite*2,baseValueSprite],[baseValueSprite*3,baseValueSprite],
+
+    [0,baseValueSprite*2],[baseValueSprite,baseValueSprite*2],
+    [baseValueSprite*2,baseValueSprite*2], [baseValueSprite*3,baseValueSprite*2],
+
+    [0,baseValueSprite*3],
+    ]
+
 let canvasEl = document.querySelector('#game');
 let ctx = canvasEl.getContext('2d');
 
